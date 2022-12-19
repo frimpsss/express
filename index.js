6d83401ab56d1351d44f6e7d0ebd263d29023db8
frimpsss <afrimpong031@gmail.com>
2022-12-19 17:19:38 +0000

    test

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import express from "express";
 import questions from './routes/getQuestions.js'
 import questionfromsection from './routes/getSectionQuestions.js'
 const app = express();
-
+const PORT = process.env.PORT || 5000
 app.use(logger);
 app.use("/api", questions)
 app.use("/api", questionfromsection)
@@ -29,6 +29,6 @@ app.all("*", logger, (req, res) => {
 
 
 
-app.listen(5000, () => {
-  console.log("Server running on port " + 5000);
+app.listen(PORT, () => {
+  console.log("Server running on port " + PORT);
 });
